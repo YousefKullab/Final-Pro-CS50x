f983bbf83f1f127ba38a843e6994977eefd44c92
Yousef kullab <the1yousef98@gmail.com>
2022-10-01 21:47:00 +0300

    Make the Structure and Navbar of website

---
+++ app.py
@@ -0,0 +1,32 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html", title="JoBa-Home")
+
+@app.route("/shop")
+def shop():
+    return render_template("shop.html", title="JoBa-Shop")
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html", title="JoBa-Blog")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="JoBa-About")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact_us.html", title="JoBa-Contact Us")
+
+
+if __name__ == "__main__":
+    
+    app.run(debug=True, port= 5000)
+    
+    
+# Software Engineer Joseph 

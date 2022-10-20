2cbfa706d724f6ec452e240631aafbc8c25bc254
Yousef kullab <the1yousef98@gmail.com>
2022-10-20 21:53:31 +0300

    Complete Contact_Us Page And Program it use js

--- static/js/main.js
+++ static/js/main.js
@@ -6,7 +6,7 @@ var smallImages = document.getElementsByClassName('small-img');
 for (let i= 0 ; i < smallImages.length; i++){
     smallImages[i].onclick = function(){
         mainImage.src = smallImages[i].src;
-    }
+    };
 }
 
 
@@ -22,11 +22,38 @@ function scrollFunction() {
   } else {
     mybutton.style.display = "none";
   }
-}
+};
 
 function topFunction() {
   document.body.scrollTop = 0; 
   document.documentElement.scrollTop = 0; 
-}
+};
 
 // Note Hover Navbar >>> Highlight
+
+
+// Send Email  
+
+function sendEmail() {
+    var params ={
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value
+    };
+
+    const serviceID = "service_ja2h26f";
+    const templateID = "template_tvg0ivi";
+    emailjs.send(serviceID, templateID, params)
+    .then(
+      res =>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your Message Sent Successfully");
+      })
+    .catch((res) => console.log(err));
+};
+
+
+

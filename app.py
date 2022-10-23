d4c7dd4e8313477cb07ab396caaca88d8af798a1
Yousef kullab <the1yousef98@gmail.com>
2022-10-24 00:24:36 +0300

    add login_required function

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 from flask import Flask, render_template, redirect
 from requests import request
 
+from helpers import login_required
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -17,22 +19,27 @@ def register():
     return render_template("register.html", title="JoBa-Register")
 
 @app.route("/shop1")
+@login_required
 def shop1():
     return render_template("shop1.html", title="JoBa-Shop1")
 
 @app.route("/shop2")
+@login_required
 def shop2():
     return render_template("shop2.html", title="JoBa-Shop2")
 
 @app.route("/shop3")
+@login_required
 def shop3():
     return render_template("shop3.html", title="JoBa-Shop3")
 
 @app.route("/product")
+@login_required
 def product():
     return render_template("product.html", title="JoBa-Product Details")
 
 @app.route("/contact")
+@login_required
 def contact():
     return render_template("contact_us.html", title="JoBa-Contact Us")
 

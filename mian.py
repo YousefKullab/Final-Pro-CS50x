8e224cdfe271124952fe2ae84b02b15ac4b659dd
Yousef kullab <the1yousef98@gmail.com>
2022-11-03 18:57:55 +0200

    edit main file

--- mian.py
+++ mian.py
@@ -1,6 +1,8 @@
 from JoBa import create_app
 
+# https://docs.python.org/3/
 # https://flask.palletsprojects.com/en/2.2.x/
+# https://flask-sqlalchemy.palletsprojects.com/en/3.0.x/
 
 app = create_app()
 

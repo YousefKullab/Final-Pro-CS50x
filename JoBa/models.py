bc21b37609f3e07e9586ccee5cbe4b5f10822d84
Yousef kullab <the1yousef98@gmail.com>
2022-11-03 18:10:39 +0200

    Update all project

---
+++ JoBa/models.py
@@ -0,0 +1,21 @@
+from . import db   # . => current package (JoBa)
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    product_name = db.Column(db.DateTime(timezone= True), default=func.now())
+    product_price = db.Column(db.Integer)
+    product_num = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    products = db.relationship('Product')
+    
+
+
---
+++ JoBa/views.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, flash, render_template,request, jsonify
+from flask_login import login_required, current_user
+from .models import Product
+from . import db
+import json
+
+views = Blueprint('views', __name__)
+
+@views.route("/")
+def home(): 
+    return render_template("home.html", title="JoBa-Home", user=current_user)
+
+@views.route("/shop1")
+@login_required
+def shop1():
+    return render_template("shop1.html", title="JoBa-Shop1", user=current_user)
+
+@views.route("/shop2")
+@login_required
+def shop2():
+    return render_template("shop2.html", title="JoBa-Shop2", user=current_user)
+
+@views.route("/shop3")
+@login_required
+def shop3():
+    return render_template("shop3.html", title="JoBa-Shop3", user=current_user)
+
+@views.route("/product")
+@login_required
+def product():
+    return render_template("product.html", title="JoBa-Product", user=current_user)
+
+@views.route("/contact_us")
+@login_required
+def contact_us():
+    return render_template("contact_us.html", title="JoBa-Contact_Us", user=current_user)
